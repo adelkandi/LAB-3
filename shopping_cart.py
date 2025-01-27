480b1549aa75794137192756c0a129b1b35e4844
adelkandi68@gmail.com <adelkandi68@gmail.com>
2025-01-27 14:02:11 -0500

    Update and creat a function shopping_cart() 0+ add a restart option.

--- shopping_cart.py
+++ shopping_cart.py
@@ -12,34 +12,55 @@ print("Welcome to the Shopping Cart Program!")
 
 
 
-price = []
-name = []
-quantity =[]
+
 
 # Step 1: Ask the user for the number of items , quantity, price
-while len(name) < 3:
-    try :
-        for i in range(3):
-            item_name = input("What is the name of this item: ")
-            name.append(item_name)
-            item_price = int(input(f'What is the price of  the item {name[i]}: '))
-            price.append(item_price)
-            item_quantity = int(input(f"What is the quantity other item {name[i]}: "))
-            quantity.append(item_quantity)
+# Creat a function named shopping_cart():
+def shopping_cart():
+    price = []
+    name = []
+    quantity = []
+    while len(name) < 3:
+        try :
+            for i in range(3):
+                item_name = input("What is the name of this item: ")
+                name.append(item_name)
+                item_price = int(input(f'What is the price of  the item {name[i]}: '))
+                price.append(item_price)
+                item_quantity = int(input(f"What is the quantity other item {name[i]}: "))
+                quantity.append(item_quantity)
 
-    except ValueError :
-        print("Please give a digit number as 1 2 3 Try again")  # if the user enters wrong data it will show him this message
+        except ValueError :
+            print("Please give a digit number as 1 2 3 Try again")  # if the user enters wrong data it will show him this message
 
-# Step 2: Calculate the total coast of the items
-total_coast = 0 # initialize the coast to value = 0
+    # Step 2: Calculate the total coast of the items
+    total_coast = 0 # initialize the coast to value = 0
 
-# Print the items with their prices in order:
-for i in range (len(price)):
-    print(f"Item {name[i]}  coast : {price[i] * quantity[i]}")
+    # Print the items with their prices in order:
+    for i in range (len(price)):
+        print(f"Item {name[i]}  coast : {price[i] * quantity[i]}")
 
-for i in range(len(price)):
-    total_coast = price[i]*quantity[i] + total_coast
+    for i in range(len(price)):
+        total_coast = price[i]*quantity[i] + total_coast
 
-print(f"The total coast: {total_coast}")  # Display the total coast of the items.
+    print(f"The total coast: {total_coast}")  # Display the total coast of the items.
+
+####################################
+
+while True:
+
+    # Main variables : name of the item, price and quantity
+    #price = []
+    #name = []
+    #quantity =[]
+
+    shopping_cart() # Run the program for the first time
+
+    restart = input("\nWould you like to shop again? (yes/no): ").lower()
+    if restart == "yes":
+        shopping_cart()
+    else:
+        print("Thank you for shopping with us!")
+        break
 
 
